2ffea0d69e6a7c69310f7a98fac3ceebb6ac8143
mahmoudAdham96 <88621466+mahmoudAdham96@users.noreply.github.com>
2022-09-11 21:10:24 +0200

    backgruond not work

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div id="app" dir="rtl">
+    <nav-Bar></nav-Bar>
+    <Header />
   </div>
 </template>
+<script>
+import navBar from "./components/navBar.vue";
+import Header from "./components/header.vue";
+export default {
+  components: {
+    navBar,
+    Header,
+  },
+};
+</script>
 
 <style lang="scss">
+@import url("http://fonts.cdnfonts.com/css/droid-arabic-naskh");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Droid Arabic Naskh", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="flex items-center justify-between w-10/12 m-auto">
+      <div class="">01</div>
+      <div
+        class=""
+        style="
+          background-image: url('https://cdn.pixabay.com/photo/2022/06/06/19/36/boat-7246847_960_720.jpg');
+        "
+      >
+        02
+      </div>
+      <div class="bg-img">03</div>
+    </div>
+    <div
+      :style="{ 'background-image': 'url(https://vuejs.org/images/logo.png)' }"
+    ></div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.bg-img {
+  background: url ("./assets/index.png");
+}
+</style>
