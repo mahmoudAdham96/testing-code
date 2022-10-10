563b37b43f52cf7169c2fef11f19e39248ebaa4d
mahmoudAdham96 <88621466+mahmoudAdham96@users.noreply.github.com>
2022-10-10 23:33:33 +0200

    Fetching data from the server

--- src/components/navSearch.vue
+++ src/components/navSearch.vue
@@ -133,7 +133,7 @@
                     <a
                       href="#"
                       class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >الصفحة الرئيسية</a
+                      ><router-link to="/">Home</router-link></a
                     >
                   </li>
                   <li aria-labelledby="dropdownNavbarLink">
--- src/views/searchView.vue
+++ src/views/searchView.vue
@@ -3,14 +3,14 @@
     <navSearch></navSearch>
     <div class="w-10/12 mx-auto mt-10">
       <div
-        class="max-w-7xl grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-10 mx-auto"
+        class="max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-10 mx-auto"
       >
         <div
-          v-for="item in 30"
-          :key="item"
-          class="max-w-sm overflow-hidden bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          v-for="item in info"
+          :key="item.id"
+          class="max-w-sm overflow-hidden bg-white rounded-lg dark:bg-gray-800"
         >
-          <div style="z-index: 1; overflow: hidden" class="w-full">
+          <div style="z-index: 1; overflow: hidden">
             <Carousel
               :per-page="1"
               :mouse-drag="true"
@@ -19,8 +19,21 @@
               :indicators="false"
               :controls="true"
             >
-              <slide tabindex="1" v-for="img in 5" :key="img">
-                <img :src="`https://picsum.photos/300/30${img}`" alt="" />
+              <!-- <slide tabindex="1" v-for="img in 5" :key="img">
+                <img
+                  class="w-full"
+                  :src="`https://picsum.photos/300/30${img}`"
+                  alt=""
+                /> -->
+              <slide tabindex="1">
+                <img class="w-full" :src="item.image" alt="" />
+              </slide>
+               <slide tabindex="2">
+                <img
+                  class="w-full"
+                  src="https://picsum.photos/300/302"
+                  alt=""
+                />
               </slide>
             </Carousel>
           </div>
@@ -29,13 +42,13 @@
               <h5
                 class="mb-1 tracking-tight text-sm text-gray-400 dark:text-white"
               >
-                Best Buy
+                {{ item.title }}
               </h5>
             </a>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              بوابة ميتا - مكالمات فيديو ذكية للمنزل مع لمس 10 بوصات
+              {{ item.description.substr(0, 60) }}...
             </p>
-            <h1 class="font-bold">$179.99</h1>
+            <h1 class="font-bold">${{ item.price }}</h1>
           </div>
         </div>
       </div>
@@ -47,22 +60,20 @@
 <script>
 import navSearch from "../components/navSearch.vue";
 import { Carousel, Slide } from "vue-carousel";
+import axios from "axios";
+
 export default {
-  // data() {
-  //   return {
-  //     settings: {
-  //       dots: true,
-  //       dotsClass: "slick-dots custom-dot-class",
-  //       edgeFriction: 0.35,
-  //       infinite: false,
-  //       speed: 500,
-  //       slidesToShow: 1,
-  //       slidesToScroll: 1,
-  //       autoplay: true,
-  //       autoplaySpeed: 2000,
-  //     },
-  //   };
-  // },
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => (this.info = response.data))
+      .catch((error) => console.log(error));
+  },
   components: {
     navSearch,
     Carousel,
@@ -71,12 +82,6 @@ export default {
 };
 </script>
 <style scoped>
-.VueCarousel-navigation-button {
-  /* color: red !important;
-  font-size: 300px !important;
-  right: 30px !important; */
-  position: relative !important;
-}
 .VueCarousel-slide {
   height: 250px;
 }
